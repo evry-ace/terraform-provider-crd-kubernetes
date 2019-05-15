7682390595e858f5e1c1336d2f5b5b3977beced7
Endre Karlson <endre.karlson@gmail.com>
2019-05-15 07:53:51 +0200

    Add onUpdate and fixes some code

--- pkg/provider/provider.go
+++ pkg/provider/provider.go
@@ -1,7 +1,6 @@
 package provider
 
 import (
-	"encoding/json"
 	"errors"
 	"log"
 	"os"
@@ -38,6 +37,7 @@ func crdResource() *schema.Resource {
 		Read:   onRead,
 		Update: onUpdate,
 		Delete: onDelete,
+		// Exists: onExists,
 
 		Importer: &schema.ResourceImporter{
 			State: schema.ImportStatePassthrough,
@@ -67,6 +67,34 @@ func crdResource() *schema.Resource {
 				Required: true,
 			},
 		},
+	}
+}
+
+func onExists(d *schema.ResourceData, m interface{}) (bool, error) {
+	crdAPIVersion := d.Get("api_version").(string)
+	crdType := strings.Split(crdAPIVersion, "/")
+
+	crdKind := d.Get("kind").(string)
+	crdName := d.Get("name").(string)
+	crdNamespace := d.Get("namespace").(string)
+
+	crdResource := groupSchema.GroupVersionResource{
+		Group:    crdType[0],
+		Version:  crdType[1],
+		Resource: strings.ToLower(crdKind) + "s",
+	}
+
+	client, _ := newClient()
+	crdInstance, err := client.Resource(crdResource).Namespace(crdNamespace).Get(crdName, metav1.GetOptions{})
+	if err != nil {
+		return false, err
+	}
+
+	if crdInstance == nil {
+		log.Println(crdInstance)
+		return false, nil
+	} else {
+		return true, nil
 	}
 }
 
@@ -111,16 +139,7 @@ func onCreate(d *schema.ResourceData, m interface{}) error {
 	}
 
 	unstructuredObj := unstructured.Unstructured{}
-
 	unstructuredObj.SetUnstructuredContent(obj)
-
-	log.Printf("Content is '%T'", unstructuredObj.Object["spec"].(map[string]interface{})["sidecarInjector"])
-
-	// serialized, _ := json.Marshal(unstructuredObj)
-	_, err = json.Marshal(unstructuredObj.Object)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	client, _ := newClient()
 
@@ -129,13 +148,12 @@ func onCreate(d *schema.ResourceData, m interface{}) error {
 	_, err = client.Resource(crdResource).Namespace(crdNamespace).Create(&unstructuredObj, metav1.CreateOptions{})
 	if err != nil {
 		panic(err)
-		return err
 	}
 
 	log.Printf("Created %s", crdName)
 
-	// return onRead(d, m)
-	return nil
+	return onRead(d, m)
+	// return nil
 }
 
 func stringMapize(i interface{}) (interface{}, error) {
@@ -143,7 +161,6 @@ func stringMapize(i interface{}) (interface{}, error) {
 	switch t := i.(type) {
 	case (map[interface{}]interface{}):
 		m := make(map[string]interface{}, len(t))
-		log.Printf("Data -> %s", t)
 
 		for k, v := range t {
 			str, ok := k.(string)
@@ -154,10 +171,13 @@ func stringMapize(i interface{}) (interface{}, error) {
 			if err != nil {
 				return nil, err
 			}
+
+			return m, nil
 		}
 	case (map[string]interface{}):
 		for k, v := range t {
 			t[k], err = stringMapize(v)
+
 			if err != nil {
 				return nil, err
 			}
@@ -185,14 +205,80 @@ func onRead(d *schema.ResourceData, m interface{}) error {
 
 	client, _ := newClient()
 
-	crdInstance, _ := client.Resource(crdResource).Namespace(crdNamespace).Get(crdName, metav1.GetOptions{})
-	m = crdInstance
+	crdInstance, err := client.Resource(crdResource).Namespace(crdNamespace).Get(crdName, metav1.GetOptions{})
+
+	log.Printf("CRD is: '%s'", crdInstance)
+	if err != nil || crdInstance == nil {
+		d.SetId("")
+		return nil
+	}
+
+	log.Printf("CRD was read %s", crdInstance.GetKind())
+
+	d.SetId(string(crdInstance.GetUID()))
+	d.Set("name", crdInstance.GetName())
+	d.Set("kind", crdInstance.GetKind())
+	d.Set("namespace", crdInstance.GetNamespace())
+	d.Set("api_version", crdInstance.GetAPIVersion())
+
+	// Read the spec and marshal it into YAML.
+	readSpec, _ := yaml.Marshal(crdInstance.Object["spec"].(map[string]interface{}))
+	d.Set("spec", readSpec)
 
 	return nil
 }
 
 func onUpdate(d *schema.ResourceData, m interface{}) error {
-	return nil
+	crdAPIVersion := d.Get("api_version").(string)
+	crdType := strings.Split(crdAPIVersion, "/")
+
+	crdKind := d.Get("kind").(string)
+	crdName := d.Get("name").(string)
+	crdNamespace := d.Get("namespace").(string)
+
+	log.Printf("Executing create for %s - %s - %s", crdAPIVersion, crdKind, crdName)
+	crdResource := groupSchema.GroupVersionResource{
+		Group:    crdType[0],
+		Version:  crdType[1],
+		Resource: strings.ToLower(crdKind) + "s",
+	}
+
+	var crdSpec map[string]interface{}
+
+	crdSpecString := d.Get("spec").(string)
+	err := yaml.Unmarshal([]byte(crdSpecString), &crdSpec)
+	if err != nil {
+		log.Printf("Spec string %s", crdSpecString)
+		log.Print(err)
+		panic("Failure decoding CRD spec")
+	}
+
+	sanitized, err := stringMapize(crdSpec)
+	if err != nil {
+		return err
+	}
+
+	obj := map[string]interface{}{
+		"apiVersion": crdAPIVersion,
+		"kind":       crdKind,
+		"metadata": map[string]string{
+			"name":      crdName,
+			"namespace": crdNamespace,
+		},
+		"spec": sanitized,
+	}
+
+	unstructuredObj := unstructured.Unstructured{}
+	unstructuredObj.SetUnstructuredContent(obj)
+
+	client, _ := newClient()
+
+	_, err = client.Resource(crdResource).Namespace(crdNamespace).Update(&unstructuredObj, metav1.UpdateOptions{})
+	if err != nil {
+		return err
+	}
+
+	return onRead(d, m)
 }
 
 func onDelete(d *schema.ResourceData, m interface{}) error {
